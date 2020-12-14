7f8fd471b5193193528b122cd64e4f8878db48dd
Byczax <byczqq136@gmail.com>
2020-12-14 11:38:07 +0100

    first layout

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main extends Thread{
+    public static void main(String[] args) {
+        Main thread = new Main();
+        thread.start();
+//        Main
+    }
+}
---
+++ src/models/Robot.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Robot {
+    String name;
+    int size;
+    int time;
+
+}
